d9d56743eae897159f0497705481753d39179c9d
David <61215846+Dav3o@users.noreply.github.com>
2021-05-30 17:07:08 +0200

    Delete Telbot directory

--- Telbot/clear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -e log.txt ]
-then
-  rm -f log.txt
-  echo 'Successfully cleared'
-fi
--- Telbot/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-apt-get install python3-tk
-pip install PyAutoGUI==0.9.52 && pip install argparse==1.4.0
